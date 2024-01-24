0486a13c34994b2aea42dd29b3ddcedb9919ec64
Alex Schittko <alex4108@live.com>
2024-01-24 08:14:23 +0000

    init can't run steamcmd

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -xeo pipefail
 
 if [[ ! "${PUID}" -eq 0 ]] && [[ ! "${PGID}" -eq 0 ]]; then
     printf "\e[0;32m*****EXECUTING USERMOD*****\e[0m\n"
@@ -17,13 +17,12 @@ if [ "${UPDATE_ON_BOOT}" = true ]; then
 printf "\e[0;32m*****STARTING INSTALL/UPDATE*****\e[0m\n"
     if [ "$(uname -a | grep arm)" != "" ]; then # if this an ARM box, use FEXBash
         FEXBash="FEXBash"
+        chmod +x /home/steam/steamcmd/steamcmd.sh
     else
         FEXBash=""
     fi
     
     ${FEXBash} su steam -c '/home/steam/steamcmd/steamcmd.sh +force_install_dir "/palworld" +login anonymous +app_update 2394010 validate +quit'
-
-    su steam -c '/home/steam/steamcmd/steamcmd.sh +force_install_dir "/palworld" +login anonymous +app_update 2394010 validate +quit'
 fi
 
 ./start.sh
